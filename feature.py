d21d0492e74a052663427885e884f25da81f6d1b
KweeKim <kweekim+seed@gmail.com>
2025-10-18 15:57:33 +0800

    update featurex to call add_number second time

--- feature.py
+++ feature.py
@@ -6,3 +6,9 @@ num1 = 10
 num2 = 5
 result = add_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is: {result}")
+
+# update featurex to create conflict 
+num1 = 55
+num2 = 10
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
